44ed5618968c2a3015c60395bc09477fc1944a46
Chedotel Benjamin <benjamin.chedotel@insa-rennes.fr>
2023-02-28 09:45:28 +0100

    bitstream generated

--- imgInPL/imgInPL.runs/impl_1/runme.sh
+++ imgInPL/imgInPL.runs/impl_1/runme.sh
@@ -20,7 +20,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='/home/user/imgInPL/imgInPL.runs/impl_1'
+HD_PWD='/home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
@@ -37,7 +37,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log design_wrapper_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source design_wrapper_wrapper.tcl -notrace
 
 
--- imgInPL/imgInPL.runs/synth_1/design_wrapper_wrapper.tcl
+++ imgInPL/imgInPL.runs/synth_1/design_wrapper_wrapper.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "/home/user/imgInPL/imgInPL.runs/synth_1/design_wrapper_wrapper.tcl"
+  variable script "/home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.runs/synth_1/design_wrapper_wrapper.tcl"
   variable category "vivado_synth"
 }
 
@@ -70,7 +70,7 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param chipscope.maxJobs 1
+set_param chipscope.maxJobs 4
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7z020clg400-1
 
@@ -78,19 +78,18 @@ set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
 set_msg_config -source 4 -id {IP_Flow 19-2162} -severity warning -new_severity info
-set_property webtalk.parent_dir /home/user/imgInPL/imgInPL.cache/wt [current_project]
-set_property parent.project_path /home/user/imgInPL/imgInPL.xpr [current_project]
+set_property webtalk.parent_dir /home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.cache/wt [current_project]
+set_property parent.project_path /home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language VHDL [current_project]
-set_property board_part www.digilentinc.com:pynq-z1:part0:1.0 [current_project]
-set_property ip_output_repo /home/user/imgInPL/imgInPL.cache/ip [current_project]
+set_property ip_output_repo /home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
-read_vhdl -library xil_defaultlib /home/user/imgInPL/imgInPL.gen/sources_1/bd/design_wrapper/hdl/design_wrapper_wrapper.vhd
-add_files /home/user/imgInPL/imgInPL.srcs/sources_1/bd/design_wrapper/design_wrapper.bd
-set_property used_in_implementation false [get_files -all /home/user/imgInPL/imgInPL.gen/sources_1/bd/design_wrapper/ip/design_wrapper_processing_system7_0_0/design_wrapper_processing_system7_0_0.xdc]
-set_property used_in_implementation false [get_files -all /home/user/imgInPL/imgInPL.gen/sources_1/bd/design_wrapper/design_wrapper_ooc.xdc]
+read_vhdl -library xil_defaultlib /home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.gen/sources_1/bd/design_wrapper/hdl/design_wrapper_wrapper.vhd
+add_files /home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.srcs/sources_1/bd/design_wrapper/design_wrapper.bd
+set_property used_in_implementation false [get_files -all /home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.gen/sources_1/bd/design_wrapper/ip/design_wrapper_processing_system7_0_0/design_wrapper_processing_system7_0_0.xdc]
+set_property used_in_implementation false [get_files -all /home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.gen/sources_1/bd/design_wrapper/design_wrapper_ooc.xdc]
 
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
@@ -101,11 +100,14 @@ OPTRACE "Adding files" END { }
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
+read_xdc /home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.srcs/constrs_1/new/long_wire.xdc
+set_property used_in_implementation false [get_files /home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.srcs/constrs_1/new/long_wire.xdc]
+
 read_xdc dont_touch.xdc
 set_property used_in_implementation false [get_files dont_touch.xdc]
 set_param ips.enableIPCacheLiteLoad 1
 
-read_checkpoint -auto_incremental -incremental /home/user/imgInPL/imgInPL.srcs/utils_1/imports/synth_1/imginfile.dcp
+read_checkpoint -auto_incremental -incremental /home/bchedote/Documents/innovR-arch-test/imgInPL/imgInPL.srcs/utils_1/imports/synth_1/imginfile.dcp
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
